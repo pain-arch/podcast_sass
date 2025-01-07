b1974e8678cbec05f2483f71ebdc086e823f1b9b
pain-arch <himulol123@gmail.com>
2025-01-07 22:38:48 +0600

    Add authentication configuration for Clerk provider

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://strong-oriole-63.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default authConfig;
